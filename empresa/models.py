704db0bf5cfd1b8e1af35e98f84195e48bbee172
Alexandre <alexandrecardosodebarros1@outlook.com>
2020-01-16 16:36:40 -0300

    fiz model da empresa

--- empresa/models.py
+++ empresa/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Empresa(models.Model):
+    
+    ramos_de_atividade = (
+        ('P','publica'),
+        ('PV','privado'),
+        ('ONG','sem fins lucrativos')
+
+    )
+    portes_tamanho = (
+        ('P','pequena'),
+        ('M','media'),
+        ('G','grande')
+    )
+
+        
+
+    nome = models.CharField(max_length=255,)
+    endereço = models.CharField(max_length=255, )
+    cnpj = models.IntergerField()
+    telefone = models.IntegerField()
+    ramo_de_atividade = models.CharField(max_length=255, choices=ramos_de_atividade)
+    porte_tamanho = models.CharField(max_length=255, choices=portes_tamanho )
+
+
+    def __str__(self):
+        return self.nome
