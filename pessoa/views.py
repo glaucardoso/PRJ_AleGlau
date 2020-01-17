5bb402b786506d957e27f8519ffb58487bb3df43
luiz <lalamail@email.com>
2020-01-17 11:10:37 -0300

    criei cadastro pessoa html ea funcao de cadastro

--- pessoa/views.py
+++ pessoa/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def cadastro_pessoa(request):
+    form = PessoaForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('/', kwargs={'msg':'Cadastrado com sucesso'})
+
+    args = {
+        'form':form
+    }
+
+    return render(request,'cadastro_pessoa.html',args)
+    
